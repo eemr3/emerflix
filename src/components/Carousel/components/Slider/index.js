1b8fe3c0a99026ac32709613932919e259735d6e
Emerson Moreira <eemr3@yahoo.com.br>
2020-08-01 00:04:21 -0300

    Ajustes no slider e db.json

--- src/components/Carousel/components/Slider/index.js
+++ src/components/Carousel/components/Slider/index.js
@@ -42,7 +42,7 @@ const Slider = ({ children }) => (
     <SlickSlider
       {...{
         dots: false,
-        infinite: true,
+        infinite: false,
         speed: 300,
         centerMode: false,
         variableWidth: true,
